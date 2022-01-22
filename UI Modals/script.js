747e8b73acd50b3b9d7f99493db7b8750e35408d
Michael Tran <mhtran.hy@gmail.com>
2022-01-21 18:36:51 -0800

    Close/open modal through buttons and overlay

--- UI Modals/script.js
+++ UI Modals/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function() {
+    // dont need .hidden because we are not selecting it, we just need the name of the class
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeModal = function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+// Adding event listeners to all buttons for opening modal
+for(let i = 0; i < btnsOpenModal.length; ++i)
+    btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+// listening for the esc key - keyboard event
+document.addEventListener('keydown', function() {
+
+})
